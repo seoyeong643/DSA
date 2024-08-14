ba953d84409cb98449effe719e2be551d345a3d5
Maia Oh <seoyeong.oh364@gmail.com>
2024-08-14 11:08:12 -0700

    Initial commit

---
+++ src/misc/classes/Main.java
@@ -0,0 +1,14 @@
+package misc.classes;
+
+public class Main {
+    public static void main(String[] args) {
+        Cookie cookieOne = new Cookie("green");
+        Cookie cookieTwo = new Cookie("blue");
+
+        cookieOne.setColor("yellow");
+
+        System.out.println(cookieOne.getColor());
+        System.out.println(cookieTwo.getColor());
+
+    }
+}
